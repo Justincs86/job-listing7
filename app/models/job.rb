c4b296045ce975bb3dd79f1d4b6ea9ac8c0694d2
justin <justinng@Justins-MacBook-Air.local>
2017-07-04 00:19:17 +0800

    add hide&publish button to job

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,15 @@ class Job < ApplicationRecord
   validates :lower_wage_bound, presence: true
   validates :upper_wage_bound, presence: true
   validates :lower_wage_bound, numericality: { greater_than: 0 }
+
+  def hide!
+    self.is_hidden = true
+    self.save
+  end
+
+  def publish!
+    self.is_hidden = false
+    self.save
+  end
+  
 end
