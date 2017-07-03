2b27422b08a25bc6a66b65cfd9074673d54e60db
justin <justinng@Justins-MacBook-Air.local>
2017-07-03 23:40:28 +0800

    add is_hidden to job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -51,7 +51,7 @@ class JobsController < ApplicationController
    private
 
    def job_params
-     params.require(:job).permit(:title, :description, :lower_wage_bound, :upper_wage_bound, :contact_email)
+     params.require(:job).permit(:title, :description, :lower_wage_bound, :upper_wage_bound, :contact_email, :is_hidden)
    end
 
 
